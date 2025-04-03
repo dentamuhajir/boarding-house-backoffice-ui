b4d1837126d2f7f72ef20bf274612103fa47ac61
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-03 20:37:23 +0700

    basic routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Header</header>
         {children}
+        <footer>Footer</footer>
       </body>
     </html>
   );

ca38d5b46c871d7631cb3e241f02041db0471f8c
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-04 13:15:03 +0700

    Create link and naviagation

--- src/app/(frontsite)/page.tsx
+++ src/app/(frontsite)/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function homepage() {
     return (
-      <h3>Homepage in frontsite</h3>
+        <>
+            <h3>Homepage in frontsite</h3>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/dashboard">CMS</Link>
+                </li>
+            </ul>
+        </>
     )
   }

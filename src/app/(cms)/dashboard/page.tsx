14e50cc55604d249918b57bd5c36df8bb77e028c
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-04 13:15:03 +0700

    Create link and naviagation

--- src/app/(cms)/dashboard/page.tsx
+++ src/app/(cms)/dashboard/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function dashboard() {
-    return <>This is dashboard section</>
+    return (
+    <>This is dashboard section
+    <ul>
+        <li>
+            <Link href="/">Home</Link>
+        </li>
+        <li>
+            <Link href="/dashboard">CMS</Link>
+        </li>
+    </ul>
+    </>
+    )
 }

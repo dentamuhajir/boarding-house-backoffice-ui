9532e9a522d6771873e96c82a03dbf654360d010
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-06 21:14:05 +0700

    Add style to header and component footer

--- src/app/(cms)/dashboard/components/header.tsx
+++ src/app/(cms)/dashboard/components/header.tsx
@@ -1,3 +1,28 @@
+import Link from 'next/link'
 export default function Header() {
-    return <> Header from components </>
+
+    return (
+    
+        <header>
+            <div className="flex gap-5 px-10">
+                <div className="flex items-center gap-3">
+                    <img className= "h-10 w-auto object-cover ..." src="https://www.maybank.co.id/-/media/Feature/Content/External-Link/logo-maybank-fin.png?h=174&iar=0&w=440&hash=509119DDFBF907B1EB89D8920E864065" />
+                    <span>My Apps</span>
+                </div>
+                <nav className="flex items-center gap-2">
+                    <Link href="/" className="hover:text-yellow-500 rounded">
+                    Home
+                    </Link>
+                    <Link href="/about" className="hover:text-yellow-500">
+                    About
+                    </Link>
+                    <Link href="/dashboard" className="hover:text-yellow-500">
+                    dashboard
+                    </Link>
+                </nav>
+            </div>
+
+        </header>
+
+    )
 }

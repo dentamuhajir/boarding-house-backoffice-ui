95b7fc9ce568723732811790abc5fa105f1198b2
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-06 21:14:05 +0700

    Add style to header and component footer

--- src/app/(cms)/dashboard/components/footer.tsx
+++ src/app/(cms)/dashboard/components/footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-    return <> Footer from components </>
+    return (
+        <footer>
+            <div className="flex gap-5 px-10 justify-center">
+                <p className="flex font-sans">2025 | @techsavvy </p>
+
+            </div>
+        </footer>
+    )
 }
--- src/app/(cms)/dashboard/page.tsx
+++ src/app/(cms)/dashboard/page.tsx
@@ -1,16 +1,9 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function dashboard() {
     return (
-    <>This is dashboard section
-    <ul>
-        <li>
-            <Link href="/">Home</Link>
-        </li>
-        <li>
-            <Link href="/dashboard">CMS</Link>
-        </li>
-    </ul>
+    <>
     </>
     )
 }

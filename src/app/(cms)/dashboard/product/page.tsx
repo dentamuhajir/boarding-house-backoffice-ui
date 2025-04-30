c8fb2d12b96895849c4aad7295ec4d2da623c2b8
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-30 19:25:42 +0700

    Add metadata title

--- src/app/(cms)/dashboard/product/page.tsx
+++ src/app/(cms)/dashboard/product/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    'title': 'List Product'
+}
 export default function product() {
     return <h3>Index Product Page children</h3>
 }

8bc54095a30805ba12679ce1b518eaecc5fae2b2
Denta Muhajir <dentamuhajir@gmail.com>
2025-04-30 21:30:47 +0700

    Aliasing models

--- src/app/(cms)/dashboard/product/page.tsx
+++ src/app/(cms)/dashboard/product/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-import { Product } from "../../../../../models/Product"
+import { Product } from "@models/Product"
 
 export const metadata: Metadata = {
     'title': 'List Product'

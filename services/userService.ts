473ffe9426c16a379c32d63e0ae789987bfeee7a
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-12 11:33:42 +0700

    implement axiosInstance inservice & try catch block in handler

--- services/userService.ts
+++ services/userService.ts
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axiosInstance from "../lib/axios"
 
 export class UserService {
     private baseUrl:string = "https://jsonplaceholder.typicode.com"
 
     async getUsers() : Promise<any[]> {
         const endpoint: string = this.baseUrl + '/users'
-        const response = await axios.get<any[]>(endpoint)
+        const response = await axiosInstance.get<any[]>(endpoint)
         return response.data
     }
 }

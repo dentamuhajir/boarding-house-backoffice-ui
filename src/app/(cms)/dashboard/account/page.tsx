80dbd61f2d0324a14d220921746c3ee9bfaa7a48
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-12 11:33:42 +0700

    implement axiosInstance inservice & try catch block in handler

--- src/app/(cms)/dashboard/account/page.tsx
+++ src/app/(cms)/dashboard/account/page.tsx
@@ -4,15 +4,20 @@ import { useEffect, useState } from "react"
 
 export default function account() {
     const [users, setUsers] = useState<any[]>([])
+    const [error, setError] = useState<string | null>(null);
     
     useEffect(() => {
         const userService = new UserService()
-        async function fetchUser(){
-            const users = await userService.getUsers()
-            setUsers(users)
-        }
-
-        fetchUser()
+        const loadUsers = async () => {
+            try {
+              const data = await userService.getUsers();
+              setUsers(data);
+            } catch (err: any) {
+              console.error('Error loading users:', err);
+              setError('Failed to load users. Please try again later.');
+            }
+          };
+          loadUsers();
     },[])
 
     console.log(users)

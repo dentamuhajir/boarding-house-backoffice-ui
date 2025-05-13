f1ad6813a2eb8d36ff703d3c3cf7e4f087eab826
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-13 21:29:43 +0700

    Set up template from codepen

---
+++ src/app/(template)/template/page.tsx
@@ -0,0 +1,7 @@
+export default function template() {
+    return (
+        <>
+            <h1>This is children</h1>
+        </>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import  "bootstrap/dist/css/bootstrap.min.css";
-import BootstrapClient from "../../components/BootstrapClient";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// import  "bootstrap/dist/css/bootstrap.min.css";
+// import BootstrapClient from "../../components/BootstrapClient";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         { children }
-        <BootstrapClient />
+        {/* <BootstrapClient /> */}
       </body>
     </html>
   );

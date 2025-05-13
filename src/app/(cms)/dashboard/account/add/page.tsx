8d045b8ad02d89a5655aa13ad3d98ac4e28db5e7
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-13 11:03:41 +0700

    Install react form and implement the basic of get data from form

---
+++ src/app/(cms)/dashboard/account/add/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+
+export default function add() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
+    return <>
+        <form onSubmit={ handleSubmit(onSubmit) }>
+        <h3>Add Users </h3>
+        
+        <div className="mb-3">
+            <label  className="form-label">Name</label>
+            <input type="text" className="form-control" id="kostName" placeholder="Enter Name" {...register('name')} />
+        </div>
+
+        <div className="mb-3">
+            <label  className="form-label">Email</label>
+            <input type="text" className="form-control" id="kostName" placeholder="Enter email" {...register('email')} />
+        </div>
+
+        <div className="mb-3">
+            <label  className="form-label">Password</label>
+            <input type="text" className="form-control" id="kostName" placeholder="Enter password" {...register('password')} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+
+    </>
+}

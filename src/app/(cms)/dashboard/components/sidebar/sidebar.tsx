599c3936d9aeb7f0728da70f3f29c6f3fddcfe32
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-14 22:21:41 +0700

    slicing sidebar template

--- src/app/(cms)/dashboard/components/sidebar/sidebar.tsx
+++ src/app/(cms)/dashboard/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from "next/image";
 const menuItems = [
     { label: 'dashboard', path: '/dashboard'},
     
@@ -6,53 +6,92 @@ const menuItems = [
 ]
 export default function sidebar() {
     return(
-        <div className="flex-shrink-0 p-3 bg-white" >
-            <a href="/" className="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-            <svg className="bi me-2" width="30" height="24"></svg>
-            <span className="fs-5 fw-semibold">Sidebar</span>
-            </a>
-            <ul className="list-unstyled ps-0">
-            <li className="mb-1">
-                <Link href='/dashboard' >
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                    Dashboard
-                    </button>
-                </Link>
-            </li>
-            <li className="mb-1">
-                <Link href='/dashboard/product'>
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#product-collapse" aria-expanded="false">
-                    Product
-                    </button>
-                </Link>
-                <div className="collapse" id="product-collapse">
-                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li>
-                        <Link href='/dashboard/product/add'> Add </Link>
-                    </li>
-                    <li>
-                        <Link href='/dashboard/product'> List </Link>
-                    </li>
-                </ul>
+        <nav className="navbar show navbar-vertical h-lg-screen navbar-expand-lg px-0 py-3 navbar-light bg-white border-bottom border-bottom-lg-0 border-end-lg" id="navbarVertical">
+            <div className="container-fluid">
+                
+                <button className="navbar-toggler ms-n2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <a className="navbar-brand py-lg-2 mb-lg-5 px-lg-6 me-0" href="#">
+                    <Image
+                        src="/images/logo/kostfinder.png"
+                        alt="Kost Finder Logo"
+                        width={100} 
+                        height={24}
+                    />
+                </a>
+                
+                <div className="navbar-user d-lg-none">
+                    
+                    <div className="dropdown">
+                        
+                        <a href="#" id="sidebarAvatar" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div className="avatar-parent-child">
+                                <img alt="Image Placeholder" src="https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" className="avatar avatar- rounded-circle"/>
+                                <span className="avatar-child avatar-badge bg-success"></span>
+                            </div>
+                        </a>
+                        
+                        <div className="dropdown-menu dropdown-menu-end" aria-labelledby="sidebarAvatar">
+                            <a href="#" className="dropdown-item">Profile</a>
+                            <a href="#" className="dropdown-item">Settings</a>
+                            <a href="#" className="dropdown-item">Billing</a>
+                            <hr className="dropdown-divider"/>
+                            <a href="#" className="dropdown-item">Logout</a>
+                        </div>
+                    </div>
                 </div>
-            </li>
-            <li className="border-top my-3"></li>
-            <li className="mb-1">
-                <Link href='/dashboard/account'>
-                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                    Account
-                    </button>
-                </Link>
-                <div className="collapse" id="account-collapse">
-                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li>
-                        <Link href='/dashboard/account/add'> Add Account</Link>
-                    </li>
-                    <li><a href="#" className="link-dark rounded">List account</a></li>
-                </ul>
+                
+                <div className="collapse navbar-collapse" id="sidebarCollapse">
+                    
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-graph-up"></i> Dashboard
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-people"></i> Users Management
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-house"></i> Kost Management
+                                <span className="badge bg-soft-primary text-primary rounded-pill d-inline-flex align-items-center ms-auto">6</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-calendar2-month"></i> Booking Management
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-credit-card-2-back"></i> Bill & Payment
+                            </a>
+                        </li>
+                    </ul>
+                    
+                    
+                    <div className="mt-auto"></div>
+                    
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-person-square"></i> Account
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <i className="bi bi-box-arrow-left"></i> Logout
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </li>
-            </ul>
-        </div>
+            </div>
+        </nav>
+   
     )
 }

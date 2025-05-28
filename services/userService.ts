c5077f37ef056ff6034258f5d75a15fe411c4241
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-29 01:11:10 +0700

    Update User service list and model and compoennt with the backend API

--- services/userService.ts
+++ services/userService.ts
@@ -2,11 +2,11 @@ import { User } from "@models/User"
 import axiosInstance from "../lib/axios"
 
 export class UserService {
-    private baseUrl:string = "https://jsonplaceholder.typicode.com"
+    private baseUrl:string = "http://localhost:8081"
 
     async getUsers() : Promise<User[]> {
         const endpoint: string = this.baseUrl + '/users'
-        const response = await axiosInstance.get<any>(endpoint)
+        const response = (await axiosInstance.get<any>(endpoint)).data
         return response.data
     }
 }

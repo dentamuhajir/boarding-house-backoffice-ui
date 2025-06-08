53eca111e89b4dc4bdc72faa76c5ce5244014a64
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-08 13:08:02 +0700

    Pass prop to paginate component to generate item number

--- src/app/(cms)/dashboard/components/paginate.tsx
+++ src/app/(cms)/dashboard/components/paginate.tsx
@@ -1,16 +1,26 @@
-export default function Paginate() {
+export default function Paginate({totalPage}: any) {
+
+
+    let pageNumber = []
+    for(let i = 0 ; i < totalPage; i++ ) {
+        pageNumber.push(
+            <li key={i} className="page-item">
+                <a className="page-link" href="#">{i + 1}</a>
+            </li>
+        )
+    }
+
     return (
                 <nav className="col d-flex align-items-center justify-content-center" aria-label="...">
                     <ul className="pagination">
                         <li className="page-item disabled">
                             <a className="page-link" href="#" tabIndex={-1} aria-disabled="true">Prev</a>
                         </li>
-                        <li className="page-item"><a className="page-link" href="#">1</a></li>
-                        {/* <li className="page-item active" aria-current="page">
+                        { pageNumber }
+                        {/* <li className="page-item"><a className="page-link" href="#">1</a></li>
+                        <li className="page-item active" aria-current="page">
                         <a className="page-link" href="#">2</a>
                         </li> */}
-                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                        <li className="page-item"><a className="page-link" href="#">3</a></li>
                         <li className="page-item">
                             <a className="page-link" href="#">Next</a>
                         </li>

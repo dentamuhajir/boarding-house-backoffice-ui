74e953e22d63411d28ce9acb36ecb5333d02b435
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-28 19:07:39 +0700

    Auth Login then save jwt to session storage

---
+++ src/services/authService.ts
@@ -0,0 +1,18 @@
+import { APIResponse } from "../types/APIResponse";
+import axiosInstance from "../lib/axios";
+import { LoginCredentials } from "@/types/auth/LoginCredentials";
+
+export class AuthService {
+    private baseUrl:string|undefined = axiosInstance.defaults.baseURL
+
+    async login(credentials: LoginCredentials): Promise<any> {
+     
+        const endpoint: string = this.baseUrl + '/api/login' 
+        const response = (await axiosInstance.post<any>(endpoint, credentials))
+        alert("here")
+        console.log(response.data.token)
+        sessionStorage.setItem("token", response.data.token);
+        console.log(response)
+        return response
+    } 
+}
